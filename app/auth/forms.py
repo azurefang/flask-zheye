3468d9b69bfdde891e3637610e8d0e50b4f7391b
Fang Tao <xidianft@gmail.com>
2014-12-10 18:04:13 +0800

    model reconstruction

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
@@ -6,22 +7,25 @@ from wtforms import ValidationError
 
 from ..models import User
 
+
 class LoginForm(Form):
-    email = StringField('email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('keep me logged in')
-    submit = SubmitField('Log In')
+    email = StringField(u'邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField(u'密码', validators=[DataRequired()])
+    remember_me = BooleanField(u'保持登录')
+    submit = SubmitField(u'登录')
 
 
 class RegistrationForm(Form):
-    firstname = StringField('姓', validators=[DataRequired()])
-    lastname = StringField('名', validators=[DataRequired()])
-    email = StringField('邮箱', validators=[DataRequired(), Email()])
-    password = PasswordField('密码', validators=[DataRequired(), EqualTo('password2', message='密码不匹配')])
-    password2 = PasswordField('重复密码', validators=[DataRequired()])
-    submit = SubmitField('注册')
+    username = StringField(u'用户名', validators=[DataRequired()])
+    email = StringField(u'邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField(u'密码', validators=[DataRequired(), EqualTo('password2', message=u'密码不匹配')])
+    password2 = PasswordField(u'重复密码', validators=[DataRequired()])
+    submit = SubmitField(u'注册')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('Email already registered')
 
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already exist')
--- app/models.py
+++ app/models.py
@@ -1,48 +1,56 @@
+# coding:utf-8
+
 from datetime import datetime
-from app import db, conn
+
 from flask import current_app
-import forgery_py
-
-
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from werkzeug.security import generate_password_hash, check_password_hash
 
-question_followers = db.Table('question_followers',
-                              db.Column(
-                                  'question_id', db.Integer, db.ForeignKey('question.id')),
-                              db.Column(
-                                  'user_id', db.Integer, db.ForeignKey('user.id'))
-                              )
+from app import db, cache
 
-topic_followers = db.Table('topic_followers',
-                           db.Column(
-                               'topic_id', db.Integer, db.ForeignKey('topic.id')),
-                           db.Column(
-                               'user_id', db.Integer, db.ForeignKey('user.id'))
-                           )
-answer_collectors = db.Table('answer_collectors',
-                             db.Column(
-                                 'answer_id', db.Integer, db.ForeignKey('answer.id')),
-                             db.Column(
-                                 'user_id', db.Integer, db.ForeignKey('user.id'))
-                             )
 
-question_topics = db.Table('question_topics',
-                           db.Column(
-                               'question_id', db.Integer, db.ForeignKey('question.id')),
-                           db.Column(
-                               'topic_id', db.Integer, db.ForeignKey('topic.id'))
-                           )
+class Permission:
+    FOLLOW = 0x01
+    ANSWER = 0x02
+    ASK = 0x04
+    MODERATE = 0x08
+    ADMINISTER = 0x80
 
-user_followers = db.Table('user_followers',
-                          db.Column(
-                              'user_id', db.Integer, db.ForeignKey('user.id')),
-                          db.Column(
-                              'follower_id', db.Integer, db.ForeignKey('user.id'))
-                          )
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW |
+                     Permission.ANSWER |
+                     Permission.ASK, True),
+            'Manager': (Permission.FOLLOW |
+                        Permission.ANSWER |
+                        Permission.ASK |
+                        Permission.MODERATE, False),
+            'Admin': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).firt()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Role:{}>'.format(self.name)
 
 
 @login_manager.user_loader
@@ -50,27 +58,104 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(UserMixin, db.Model):
+class UserFollower(db.Model):
+    __tablename__ = 'userfollowers'
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    followed_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
 
+
+class QuestionFollower(db.Model):
+    __tablename__ = 'questionfollowers'
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    question_id = db.Column(
+        db.Integer, db.ForeignKey('questions.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+
+
+class TopicFollower(db.Model):
+    __tablename__ = 'topicfollowers'
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    topic_id = db.Column(
+        db.Integer, db.ForeignKey('topics.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+
+
+class AnswerCollector(db.Model):
+    __tablename__ = 'answercollectors'
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    answer_id = db.Column(
+        db.Integer, db.ForeignKey('answers.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+
+
+class QuestionTopic(db.Model):
+    __tablename__ = 'questiontopics'
+    question_id = db.Column(
+        db.Integer, db.ForeignKey('questions.id'), primary_key=True)
+    topic_id = db.Column(
+        db.Integer, db.ForeignKey('topics.id'), primary_key=True)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(8))
-    lastname = db.Column(db.String(10))
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
-    domain = db.Column(db.String(30))
-    email = db.Column(db.String(30), unique=True)
+    email = db.Column(db.String(30), unique=True, index=True)
     about_me = db.Column(db.Text)
     member_since = db.Column(db.DateTime, default=datetime.utcnow())
     last_seen = db.Column(db.DateTime, default=datetime.utcnow())
     confirmed = db.Column(db.Boolean, default=False)
-
-    comment = db.relationship('Comment', backref='owner', lazy='dynamic')
-    question = db.relationship('Question', backref='owner', lazy='dynamic')
-    answer = db.relationship('Answer', backref='owner', lazy='dynamic')
-    followers = db.relationship('User', secondary=user_followers, primaryjoin=id == user_followers.c.user_id,
-                                secondaryjoin=id == user_followers.c.follower_id, backref=db.backref('followed_users', lazy='dynamic'))
-    followed = db.relationship('User', secondary=user_followers, primaryjoin=id == user_followers.c.follower_id,
-                               secondaryjoin=id == user_followers.c.user_id, backref=db.backref('fans', lazy='dynamic'))
+    followed_users = db.relationship('UserFollower',
+                                     foreign_keys=[UserFollower.user_id],
+                                     backref=db.backref(
+                                         'follower', lazy='joined'),
+                                     lazy='dynamic',
+                                     cascade='all, delete-orphan')
+    followed_questions = db.relationship('QuestionFollower',
+                                         foreign_keys=[
+                                             QuestionFollower.user_id],
+                                         backref=db.backref(
+                                             'follower', lazy='joined'),
+                                         lazy='dynamic',
+                                         cascade='all, delete-orphan')
+    followed_topics = db.relationship('TopicFollower',
+                                      foreign_keys=[TopicFollower.user_id],
+                                      backref=db.backref(
+                                          'follower', lazy='joined'),
+                                      lazy='dynamic',
+                                      cascade='all, delete-orphan')
+    followers = db.relationship('UserFollower',
+                                foreign_keys=[UserFollower.followed_id],
+                                backref=db.backref(
+                                    'followed_user', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+    collected_answers = db.relationship('AnswerCollector',
+                                        foreign_keys=[AnswerCollector.user_id],
+                                        backref=db.backref(
+                                            'collector', lazy='joined'),
+                                        lazy='dynamic',
+                                        cascade='all, delete-orphan')
+    comments = db.relationship('Comment', backref='owner', lazy='dynamic')
+    questions = db.relationship('Question', backref='owner', lazy='dynamic')
+    answers = db.relationship('Answer', backref='owner', lazy='dynamic')
     activity = db.relationship('Activity', backref='owner', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['ZHEYE_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            else:
+                self.role = Role.query.filter_by(default=True).first()
 
     @property
     def password(self):
@@ -103,88 +188,154 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
-    def __repr__(self):
-        return "<User {}>".format(self.domain)
+    def follow_user(self, user):
+        if not self.is_following_user(user):
+            f = UserFollower(follower=self, followed_user=user)
+            db.session.add(f)
 
-    def follow(self, user):
-        if not self.is_following(user):
-            self.followed.append(user)
+    def unfollow_user(self, user):
+        f = self.followed_users.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
 
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.followed.remove(user)
+    def follow_topic(self, topic):
+        if not self.is_following_topic(topic):
+            f = TopicFollower(follower=self, followed_topic=topic)
+            db.session.add(f)
 
-    def is_following_user(self, user):
-        return user in self.followed
+    def unfollow_topic(self, topic):
+        f = self.followed_topics.filter_by(topic_id=topic.id).first()
+        if f:
+            db.session.delete(f)
+
+    def follow_question(self, question):
+        if not self.is_following_question(question):
+            f = QuestionFollower(follower=self, followed_question=question)
+            db.session.add(f)
+
+    def unfollow_question(self, question):
+        f = self.followed_questions.filter_by(question_id=question.id).first()
+        if f:
+            db.session.delete(f)
+
+    def collect_answer(self, answer):
+        if not self.is_collecting_answer(answer):
+            c = AnswerCollector(collector=self, collected_answer=answer)
+            db.session.add(c)
+
+    def uncollect_answer(self, answer):
+        c = self.collected_answers.filter_by(answer_id=answer.id).first()
+        if c:
+            db.session.delete(c)
 
     def is_followed_by(self, user):
-        return self in user.followed
+        return self.followers.filter_by(user_id=user.id).first() is not None
+
+    def is_following_user(self, user):
+        return self.followed_users.filter_by(followed_id=user.id).first() is not None
 
     def is_following_topic(self, topic):
-        return topic in self.followed_topics.all()
+        return self.followed_topics.filter_by(topic_id=topic.id).first() is not None
 
     def is_following_question(self, question):
-        return question in self.followed_questions.all()
+        return self.followed_questions.filter_by(question_id=question.id).first() is not None
 
-    @staticmethod
-    def generate_fake(count=100):
-        #from sqlalchemy.ext import IntegrityError
-        from random import seed
+    def is_collecting_answer(self, answer):
+        return self.collected_answers.filter_by(answer_id=answer.id).first() is not None
 
-        seed()
-        for i in range(count):
-            u = User(email=forgery_py.internet.email_address(),
-                    firstname=forgery_py.internet.user_name(),
-                    lastname=forgery_py.internet.first_name(),
-                    password=forgery_py.lorem_ipsum.word(),
-                    member_since=forgery_py.date.date(True),
-                    )
-            db.session.add(u)
-            db.session.commit()
+    def get_followed_users(self):
+        return [rel.follower for rel in self.followers]
 
+    def __repr__(self):
+        return "<User:{}>".format(self.username)
 
 
 class Topic(db.Model):
+    __tablename__ = 'topics'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
-    followers = db.relationship('User', secondary=topic_followers, backref=db.backref(
-        'followed_topics', lazy='dynamic'))
+    desc = db.Column(db.Text)
+    followers = db.relationship('TopicFollower',
+                                foreign_keys=[TopicFollower.topic_id],
+                                backref=db.backref(
+                                    'followed_topic', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+    questions = db.relationship('QuestionTopic',
+                                foreign_keys=[QuestionTopic.topic_id],
+                                backref=db.backref(
+                                    'topic', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
     activity = db.relationship('Activity', backref='topic', lazy='dynamic')
+
+    def get_followers(self):
+        return [rel.follower for rel in self.followers]
+
+    def get_questions(self):
+        return [rel.question for rel in self.questions]
 
     def __repr__(self):
         return "<Topic:{}>".format(self.name)
 
 
 class Question(db.Model):
+    __tablename__ = 'questions'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30))
-    time = db.Column(db.DateTime, default=datetime.utcnow())
     content = db.Column(db.Text)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     anonymous = db.Column(db.Boolean)
-    followers = db.relationship('User', secondary=question_followers, backref=db.backref(
-        'followed_questions', lazy='dynamic'))
-    topics = db.relationship('Topic', secondary=question_topics, backref=db.backref(
-        'related_questions', lazy='dynamic'))
-    answers = db.relationship('Answer', backref='quesion', lazy='dynamic')
-
+    followers = db.relationship('QuestionFollower',
+                                foreign_keys=[QuestionFollower.question_id],
+                                backref=db.backref(
+                                    'followed_question', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+    topics = db.relationship('QuestionTopic',
+                             foreign_keys=[QuestionTopic.question_id],
+                             backref=db.backref(
+                                 'question', lazy='joined'),
+                             lazy='dynamic',
+                             cascade='all, delete-orphan')
+    answers = db.relationship('Answer', backref='question', lazy='dynamic')
     activity = db.relationship('Activity', backref='question', lazy='dynamic')
+
+    def has_topic(self, topic):
+        return self.topics.filter_by(topic_id=topic.id).first() is not None
+
+    def add_topic(self, topic):
+        if not self.has_topic(topic):
+            r = QuestionTopic(question=self, topic=topic)
+            db.session.add(r)
+
+    def remove_topic(self, topic):
+        c = self.topics.filter_by(topic_id=topic.id).first()
+        if c:
+            db.session.delete(c)
+
+    def get_topics(self):
+        return [rel.topic for rel in self.topics]
 
     def __repr__(self):
         return "<Question:{}>".format(self.id)
 
 
 class Answer(db.Model):
+    __tablename__ = 'answers'
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, default=datetime.utcnow())
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
     content = db.Column(db.Text)
     activity = db.relationship('Activity', backref='answer', lazy='dynamic')
-    # liker
-    # hater
-    collectors = db.relationship('User', secondary=answer_collectors, backref=db.backref(
-        'collected_answers', lazy='dynamic'))
+    collectors = db.relationship('AnswerCollector',
+                                 foreign_keys=[AnswerCollector.answer_id],
+                                 backref=db.backref(
+                                     'collected_answer', lazy='joined'),
+                                 lazy='dynamic',
+                                 cascade='all, delete-orphan')
 
     comment = db.relationship('Comment', backref='answer', lazy='dynamic')
 
@@ -193,13 +344,15 @@ class Answer(db.Model):
 
 
 class Comment(db.Model):
+    __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, default=datetime.utcnow())
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
     content = db.Column(db.Text)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id'))
-    # liker
-    # father
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    answer_id = db.Column(db.Integer, db.ForeignKey('answers.id'))
+
+    def __repr__(self):
+        return "<Comment:{}>".format(self.id)
 
 
 class Activity(db.Model):
@@ -208,16 +361,19 @@ class Activity(db.Model):
     1:answer question;2:follow question;3:ask question;4:follow topic
     '''
     move = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
-    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
-    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id'))
-    time = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
+    answer_id = db.Column(db.Integer, db.ForeignKey('answers.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
 
 
 class Message(db.Model):
-    __tablename__ = 'message'
+    __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     read = db.Column(db.Boolean, default=False)
     content = db.Column(db.Text)
+
+    def __repr__(self):
+        return "<Message:{}>".format(self.id)
